14d0471db62a2c3c6cb89367deed56872a5acbb3
mavhc <mark.scholes@gmail.com>
2023-10-20 14:23:22 +0100

    Add files via upload

---
+++ add_worksheet_test.ps1
@@ -0,0 +1,12 @@
+﻿Import-Module -Name ImportExcel
+
+$template = Open-ExcelPackage "scienceTemplate.xlsx"
+$wsScience = $template.Workbook.Worksheets["Science"]
+
+$destination = "test.xlsx"
+
+$excel = Open-ExcelPackage $destination
+
+$ws = Add-Worksheet -ExcelPackage $excel -WorkSheetname "Science" -ClearSheet -CopySource $wsScience
+
+Close-ExcelPackage $excel
